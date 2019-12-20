2402c06d368d6a168aea35132680493a67252d43
Juliano <llara.juliano@gmail.com>
2019-12-20 20:08:01 -0300

    prompt intall android

--- src/app/auth/page/login/login.page.ts
+++ src/app/auth/page/login/login.page.ts
@@ -7,7 +7,6 @@ import { AuthService } from 'src/app/core/services/auth.service';
 import { AuthProvider } from 'src/app/core/services/auth.types';
 import { OverlayService } from 'src/app/core/services/overlay.service';
 import { UsersService } from 'src/app/tabs/services/users.service';
-import { tap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-login',
@@ -24,6 +23,11 @@ export class LoginPage implements OnInit {
   };
   private nameControl = new FormControl('', [Validators.required, Validators.minLength(3)]);
 
+  prompt: any;
+  installButton: any;
+  closeButton: any;
+  installEvent: any;
+
   constructor(
     private authService: AuthService,
     private usersService: UsersService,
@@ -32,15 +36,61 @@ export class LoginPage implements OnInit {
     private route: ActivatedRoute,
     private overlayService: OverlayService,
     private router: Router
-  ) {}
+  ) {  }
 
   ngOnInit(): void {
+
+    const prompt = document.querySelector('.prompt') as HTMLElement;
+    const installButton = prompt.querySelector('.prompt__install') as HTMLElement;
+    //const closeButton = prompt.querySelector('.prompt__close') as HTMLElement;
+    let installEvent: any;
+
+    window.addEventListener('beforeinstallprompt', (event) => {
+      event.preventDefault();
+      
+      // if no localStorage is set, first time visitor
+      if (!this.getVisited()) {
+        // show the prompt banner
+        prompt.style.display = 'block';
+    
+        // store the event for later use
+        installEvent = event;
+      }
+    });
+
+    installButton.addEventListener('click', () => {
+      // hide the prompt banner
+      prompt.style.display = 'none';
+    
+      // trigger the prompt to show to the user
+      installEvent.prompt();
+    
+      // check what choice the user made
+      installEvent.userChoice.then((choice: { outcome: string; }) => {
+        // if the user declined, we don't want to show the button again
+        // set localStorage to true
+        if (choice.outcome !== 'accepted') {
+          //this.setVisited();
+        }
+    
+        installEvent = null;
+      });
+    });
+
     this.createForm();
   }
   
   ionViewWillEnter(): void {
       this.authService.isAuthenticated.subscribe(
         logged => this.redirect(logged))
+  }
+
+  getVisited() {
+    return localStorage.getItem('install-prompt');
+  }
+  
+  setVisited() {
+    localStorage.setItem('install-prompt', 'true');
   }
 
   private redirect(logged: boolean) {
